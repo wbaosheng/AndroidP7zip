2ea232da8c4b0395aead92b7c7ce8516188b9fa1
wangbaosheng <wangbaosheng@360.cn>
2017-03-17 18:51:18 +0800

    remove duplicate jni

--- P7zip/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_PLATFORM := android-8
-#APP_ABI := armeabi armeabi-v7a x86
-#APP_STL := gnustl_static
--- P7zip/jni/p7zip/p7zip_9.38.1/CPP/7zip/QMAKE/all.pro
+++
@@ -1,5 +0,0 @@
-
-TEMPLATE = subdirs
-SUBDIRS = 7ZA \
-          7ZR \
-          test_emul
